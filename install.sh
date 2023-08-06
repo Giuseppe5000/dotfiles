db55e23adec27e4293efa3abb1f602d7e810c157
Giuseppe Tutino <giuseppetutino1@gmail.com>
2023-08-06 19:24:39 +0200

    Install file added

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Paru
+sudo pacman -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+cd ..
+
+# Install all packets
+paru -S --needed $(sed -e '/^\s*#.*$/d' -e '/^\s*$/d' pacmanpkg.txt)
+
+# Configs
+mv .config/* $HOME/.config
+
+# Cursor
+mv .icons $HOME
+
+# Wallpaper
+mv background.jpg $HOME/Immagini
+
+# ZSH
+mv .zshenv $HOME
+mv .zshrc $HOME
