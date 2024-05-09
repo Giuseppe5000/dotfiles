1edf8b7fe78048e318998b6e7a8795fb44f5bd97
Giuseppe Tutino <giuseppetutino1@gmail.com>
2024-05-09 19:39:15 +0200

    Install script POSIX-compliant

--- install/debian-install.sh
+++ install/debian-install.sh
@@ -1,13 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
-cd $(dirname $0)
+cd "$(dirname "$0")" || exit
 
 # Install all packets
-su -c 'apt update && apt -y install $(sed -e '/^\s*#.*$/d' -e '/^\s*$/d' aptpkg.txt)'
-if [[ $? > 0 ]]
-then
-    exit
-fi
+su -c "apt update && apt -y install $(sed -e '/^\s*#.*$/d' -e '/^\s*$/d' aptpkg.txt)" || exit
 
 cd ..
 
@@ -15,64 +11,64 @@ cd ..
 for dir in .config/*
 do
     if [ -d "$dir" ] ; then
-        mkdir -p $HOME/$dir
-        for conf in $dir/*
+        mkdir -p "$HOME"/"$dir"
+        for conf in "$dir"/*
         do
-            ln -s $PWD/$conf $HOME/$conf
+            ln -s "$PWD"/"$conf" "$HOME"/"$conf"
         done
     fi
 done
 
 # Emacs config
-ln -s $PWD/.emacs.org $HOME/.emacs.org
+ln -s "$PWD"/.emacs.org "$HOME"/.emacs.org
 
 # Vim config
-ln -s $PWD/.vimrc $HOME/.vimrc
+ln -s "$PWD"/.vimrc "$HOME"/.vimrc
 
 # GnuPG
-mkdir -p $HOME/.gnupg
-ln -s $PWD/.gnupg/gpg-agent.conf $HOME/.gnupg/gpg-agent.conf
+mkdir -p "$HOME"/.gnupg
+ln -s "$PWD"/.gnupg/gpg-agent.conf "$HOME"/.gnupg/gpg-agent.conf
 
 # NetworkManager MAC randomization conf
 # su -c 'mv etc/NetworkManager/conf.d/wifi_rand_mac.conf /etc/NetworkManager/conf.d/'
 
 # Cursor
-mv .icons $HOME
+mv .icons "$HOME"
 
 # Create user dirs
 xdg-user-dirs-update
 
 # Wallpapers
-mv Wallpapers $HOME/Pictures
+mv Wallpapers "$HOME"/Pictures
 
 # ZSH
-ln -s $PWD/.zshenv $HOME/.zshenv
-ln -s $PWD/.zshrc $HOME/.zshrc
-ln -s $PWD/.p10k.zsh $HOME/.p10k.zsh
+ln -s "$PWD"/.zshenv "$HOME"/.zshenv
+ln -s "$PWD"/.zshrc "$HOME"/.zshrc
+ln -s "$PWD"/.p10k.zsh "$HOME"/.p10k.zsh
 
 # Firefox user.js
 timeout 10s firefox --headless # Gen profile folder
-PROFILE_PATH=$(grep '^Path=.*default-esr$' $HOME/.mozilla/firefox/profiles.ini | cut -c 6-)
-ln -s $PWD/user.js $HOME/.mozilla/firefox/$PROFILE_PATH/user.js
+PROFILE_PATH=$(grep '^Path=.*default-esr$' "$HOME"/.mozilla/firefox/profiles.ini | cut -c 6-)
+ln -s "$PWD"/user.js "$HOME"/.mozilla/firefox/"$PROFILE_PATH"/user.js
 
 # Greenclip (i3)
-mkdir -p $HOME/.local/bin
-wget https://github.com/erebe/greenclip/releases/download/v4.2/greenclip -O $HOME/.local/bin/greenclip
-chmod +x $HOME/.local/bin/greenclip
+mkdir -p "$HOME"/.local/bin
+wget https://github.com/erebe/greenclip/releases/download/v4.2/greenclip -O "$HOME"/.local/bin/greenclip
+chmod +x "$HOME"/.local/bin/greenclip
 
 # Cliphist (sway)
-wget https://github.com/sentriz/cliphist/releases/download/v0.1.2/v0.1.2-linux-amd64 -O $HOME/.local/bin/cliphist
-chmod +x $HOME/.local/bin/cliphist
+wget https://github.com/sentriz/cliphist/releases/download/v0.1.2/v0.1.2-linux-amd64 -O "$HOME"/.local/bin/cliphist
+chmod +x "$HOME"/.local/bin/cliphist
 
 # Powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$HOME"/.powerlevel10k
 
 # Nerd Fonts
-mkdir -p $HOME/.local/share/fonts
+mkdir -p "$HOME"/.local/share/fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/IosevkaTerm.tar.xz
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/DejaVuSansMono.tar.xz
-tar xvf IosevkaTerm.tar.xz -C $HOME/.local/share/fonts
-tar xvf DejaVuSansMono.tar.xz -C $HOME/.local/share/fonts
+tar xvf IosevkaTerm.tar.xz -C "$HOME"/.local/share/fonts
+tar xvf DejaVuSansMono.tar.xz -C "$HOME"/.local/share/fonts
 rm IosevkaTerm.tar.xz DejaVuSansMono.tar.xz
 fc-cache -f
 
